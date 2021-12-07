1ebf79ed515128b9638f245796f089739fc81825
qwerfdsf <240417lexa@mail.ru>
2021-12-08 01:46:02 +0300

    add new version Testing

---
+++ areal-test-main/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <th>ID:</th>
+    <th>Имя: </th>
+    <th>Фамилия: </th>
+    <th>Email: </th>
+    <hr>
+    <ListApi v-for="list in listparam" :key="list" :list="list"/>
+  </div>
+</template>
+
+<script>
+import ListApi from '@/components/ListApi'
+export default {
+  name: 'App',
+  data(){
+    return{
+      listparam:[]
+    }
+  },
+  mounted() {
+    fetch('https://my.api.mockaroo.com/test_api.json?key=91233870')
+        .then(response => response.json())
+        .then(json => {this.listparam = json})
+  },
+  components: {
+    ListApi
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+th{
+  text-align: center;
+  font-size: 18px;
+  padding: 15px;
+}
+</style>
---
+++ areal-test-main/src/components/ListApi.vue
@@ -0,0 +1,27 @@
+<template>
+      <tr>
+        <td> {{ list['id'] }}</td>
+        <td> {{ list['first_name'] }}</td>
+        <td> {{ list['last_name'] }}</td>
+        <td> {{ list['email'] }}</td>
+      </tr>
+</template>
+
+<script>
+export default {
+  props: ["list"],
+  data() {
+    return {
+    }
+  },
+}
+</script>
+
+<style>
+td {
+  border: 2px solid grey;
+  padding: 10px;
+  border-radius: 5px;
+}
+
+</style>
